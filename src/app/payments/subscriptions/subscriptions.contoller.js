4d3e14dca31448d587323c49c2c7996e0d41d652
Mikhail Mikhalev <Mikhail Mikhalev>
2016-09-06 16:24:52 +0300

    Сверстана секция для выбора способа оплаты

--- src/app/payments/subscriptions/subscriptions.contoller.js
+++ src/app/payments/subscriptions/subscriptions.contoller.js
@@ -6,5 +6,17 @@
 
     function subscriptionsController() {
         var vm = this;
+
+        vm.paymentsMethods = paymentsMethods;
     }
+
+    var paymentsMethods = [
+        { name: 'Visa', cover: 'assets/images/sprite.payment-cards.png' },
+        { name: 'Яндекс.Деньги', cover: 'assets/images/sprite.payment-yandexmoney.png' },
+        { name: 'PayPal', cover: 'assets/images/sprite.payment-paypal.png' },
+        { name: 'WebMoney', cover: 'assets/images/sprite.payment-webmoney.png' },
+        { name: 'SMS', cover: '', desc: 'Только для России' },
+        { name: 'QIWI', cover: 'assets/images/sprite.payment-qiwi.png' },
+        { name: 'Подарочный код', cover: '' },
+    ];
 })();
