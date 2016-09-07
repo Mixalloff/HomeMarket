017cb05a6b3d1e01e0e5c921785849ff8cf19298
Mikhail Mikhalev <Mikhail Mikhalev>
2016-09-07 14:08:25 +0300

    Рефакторинг стилей, шрифтов и кода

--- src/app/payments/subscriptions/subscriptions.contoller.js
+++ src/app/payments/subscriptions/subscriptions.contoller.js
@@ -30,7 +30,6 @@
         vm.isAddDiscount = false;
         vm.calculateCurrentAmount = calculateCurrentAmount;
 
-
         // Установка выбранного способа оплаты
         function setCurrentPaymentMethod(method) {
             if (!isMethodDisabled(method)) {
@@ -106,20 +105,4 @@
         ],
         discountSubscriptionPrice: 150
     }
-
-    // var paymentsMethods = [
-    //     { id: 'visa', name: 'Visa', active_cover: 'assets/images/sprite.payment-cards.png', inactive_cover: 'assets/images/sprite.payment-cards-bw.png' },
-    //     { id: 'ymoney', name: 'Яндекс.Деньги', active_cover: 'assets/images/sprite.payment-yandexmoney.png', inactive_cover: 'assets/images/sprite.payment-yandexmoney-bw.png' },
-    //     { id: 'paypal', name: 'PayPal', active_cover: 'assets/images/sprite.payment-paypal.png', inactive_cover: 'assets/images/sprite.payment-paypal-bw.png' },
-    //     { id: 'webmoney', name: 'WebMoney', active_cover: 'assets/images/sprite.payment-webmoney.png', inactive_cover: 'assets/images/sprite.payment-webmoney-bw.png' },
-    //     { id: 'sms', name: 'SMS', active_cover: '', inactive_cover: '', desc: 'Только для России' },
-    //     { id: 'qiwi', name: 'QIWI', active_cover: 'assets/images/sprite.payment-qiwi.png', inactive_cover: 'assets/images/sprite.payment-qiwi-bw.png'  },
-    //     { id: 'giftcode', name: 'Подарочный код', active_cover: '' },
-    // ];
-
-    // var tariffs = [
-    //     { id: 'two_years', duration: '2 года', amount: 2880, month_pay: 120, card_cover: 'assets/images/discount_card.png' },
-    //     { id: 'one_year', duration: '1 год', amount: 1500, month_pay: 125, card_cover: 'assets/images/discount_card.png' },
-    //     { id: 'half_year', duration: '6 месяцев', amount: 780, month_pay: 130, card_cover: 'assets/images/discount_card.png' },
-    // ]
 })();
